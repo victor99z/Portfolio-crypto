f2def9099daa49b337336553423ca393a32c1b39
jubileu99 <victor_luizbernardes5@outlook.com>
2019-05-25 17:56:12 -0300

    SetRizeble adicionado

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 444, 416));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
     public static void main(String[] args) {
