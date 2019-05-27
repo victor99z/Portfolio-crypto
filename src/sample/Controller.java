2ff0ff1c1b7589e8522508a44703913648c73b1d
jubileu99 <victor_luizbernardes5@outlook.com>
2019-05-27 14:34:30 -0300

    Implentando botao funcional!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,17 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    private JFXButton btn;
+
+    @FXML
+    void display(ActionEvent event) {
+        System.out.println("SHOE DE BOLA");
+    }
 
 }
