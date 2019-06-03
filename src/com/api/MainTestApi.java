4ca5bb72a8f3a47bcf37e4139161671e92a2d027
jubileu99 <victor_luizbernardes5@outlook.com>
2019-06-03 18:03:07 -0300

    Implementado pacote da api

--- src/com/api/MainTestApi.java
+++ src/com/api/MainTestApi.java
@@ -3,6 +3,6 @@ package com.api;
 public class MainTestApi {
     public static void main(String[] args) {
         ApiReader response1 = new ApiReader("https://api.coincap.io/v2/markets/?exchangeId=binance&baseSymbol=TUSD");
-        System.out.println(response1.toString());
+        System.out.println(response1);
     }
 }
