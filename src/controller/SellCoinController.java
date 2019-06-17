b1b71fae2b91ecef4c3fda651a3c9a08d4191a87
Draylon <37211037+Draylon@users.noreply.github.com>
2019-06-17 01:06:39 -0300

    Ultimos commits

--- src/controller/SellCoinController.java
+++ src/controller/SellCoinController.java
@@ -3,6 +3,7 @@ package controller;
 import api.DataExchange;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextField;
 import database.CoinInfo;
 import database.UserCoin;
@@ -10,6 +11,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
@@ -19,6 +22,9 @@ public class SellCoinController extends ControllerClassType{
     private JFXListView<GridPane> coinDisplay;
     @FXML
     private StackPane coinStackPane;
+
+    @FXML
+    private Label currentSliderValue;
 
     private UserCoin userCoins;
     public void setUserCoinObject(UserCoin uc){ this.userCoins = uc;}
@@ -32,19 +38,38 @@ public class SellCoinController extends ControllerClassType{
         ObservableList<GridPane> listCoins = FXCollections.observableArrayList();
         if(getApiData()!= null && getApiData().isDataReady()) {
             for (CoinInfo ci : userCoins.arrayCoins) {
-                GridPane p = new GridPane();
+                if (!ci.idCoin.equals("TUSD")){
+                    GridPane p = new GridPane();
+                    p.setMinHeight(100);
+                    /*JFXTextField t = new JFXTextField();
+                    t.setPromptText("Quantia");*/
+                    JFXSlider t = new JFXSlider();
+                    t.setMinWidth(220);
+                    t.setMax(ci.qtd);
+                    t.setValue(0);
+                    t.setMin(0);
+                    t.setMinorTickCount(0);
+                    t.setMajorTickUnit(0.5);
+                    t.setShowTickLabels(true);
+                    t.setBlockIncrement(0.00000001);
+                    t.setOnDragDetected(event -> {
+                        currentSliderValue.setText(String.valueOf(t.getValue()));
+                    });
+                    t.setOnMouseDragged(event -> {
+                        currentSliderValue.setText(String.valueOf(t.getValue()));
+                    });
+                    t.setOnMouseClicked(event -> {
+                        currentSliderValue.setText(String.valueOf(t.getValue()));
+                    });
 
-                JFXTextField t = new JFXTextField();
-                t.setPromptText("Quantia");
-                JFXButton b = new JFXButton();
-                b.setText("Vender");
-                b.setOnAction(event -> {
-                    if (t.getText().length() > 0)
+                    JFXButton b = new JFXButton();
+                    b.setText("Vender");
+                    b.setOnAction(event -> {
                         try {
-                            CoinInfo nc = new CoinInfo(ci.idCoin, Double.valueOf(ci.buyPrice), Double.valueOf(t.getText()));
-                            if(userCoins.sellCoin(nc,getApiData().getDataArray().data)){
+                            CoinInfo nc = new CoinInfo(ci.idCoin, Double.valueOf(ci.buyPrice), Double.valueOf(t.getValue()));
+                            if (userCoins.sellCoin(nc, getApiData().getDataArray().data)) {
                                 //feito
-                            }else {
+                            } else {
                                 messageDialog(coinStackPane, "Operação malsucedida", "Erro");
                             }
                         } catch (Exception e) {
@@ -52,15 +77,16 @@ public class SellCoinController extends ControllerClassType{
                         /*HomeController parentController = (HomeController) getParentController();
                         parentController.updateJSON();*/
                         }
-                });
+                    });
 
-                p.setAlignment(Pos.CENTER);
-                p.setHgap(10);
-                p.addColumn(1, new Text(ci.idCoin));
-                p.addColumn(2, t);
-                p.addColumn(3, b);
-                c++;
-                listCoins.add(p);
+                    p.setAlignment(Pos.CENTER);
+                    p.setHgap(10);
+                    p.addColumn(1, new Text(ci.idCoin));
+                    p.addColumn(2, t);
+                    p.addColumn(3, b);
+                    c++;
+                    listCoins.add(p);
+                }
             }
         }
         coinDisplay.setItems(listCoins);
--- src/controller/WithdrawCoinController.java
+++ src/controller/WithdrawCoinController.java
@@ -3,6 +3,7 @@ package controller;
 import api.DataExchange;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextField;
 import database.CoinInfo;
 import database.UserCoin;
@@ -10,6 +11,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
@@ -19,6 +22,9 @@ public class WithdrawCoinController extends ControllerClassType{
     private JFXListView<GridPane> coinDisplay;
     @FXML
     private StackPane coinStackPane;
+
+    @FXML
+    private Label currentSliderValue;
 
     private UserCoin userCoins;
     public void setUserCoinObject(UserCoin uc){ this.userCoins = uc;}
@@ -32,16 +38,35 @@ public class WithdrawCoinController extends ControllerClassType{
         ObservableList<GridPane> listCoins = FXCollections.observableArrayList();
         if(getApiData()!= null && getApiData().isDataReady()) {
             for (DataExchange ci : getApiData().getDataArray().data) {
-                if(ci.baseSymbol.equals("USDT")){
+                if(ci.baseSymbol.equals("TUSD")){
                     GridPane p = new GridPane();
-                    JFXTextField t = new JFXTextField();
-                    t.setPromptText("Quantia");
+                    p.setMinHeight(100);
+                    /*JFXTextField t = new JFXTextField();
+                    t.setPromptText("Quantia");*/
+                    JFXSlider t = new JFXSlider();
+                    t.setMinWidth(220);
+                    t.setMax(userCoins.getCoinBySymbol("TUSD").qtd);
+                    t.setValue(0);
+                    t.setMin(0);
+                    t.setMinorTickCount(0);
+                    t.setMajorTickUnit(0.5);
+                    t.setShowTickLabels(true);
+                    t.setBlockIncrement(0.00000001);
+                    t.setOnDragDetected(event -> {
+                        currentSliderValue.setText(String.valueOf(t.getValue()));
+                    });
+                    t.setOnMouseDragged(event -> {
+                        currentSliderValue.setText(String.valueOf(t.getValue()));
+                    });
+                    t.setOnMouseClicked(event -> {
+                        currentSliderValue.setText(String.valueOf(t.getValue()));
+                    });
+
                     JFXButton b = new JFXButton();
                     b.setText("Retirar");
                     b.setOnAction(event -> {
-                        if (t.getText().length() > 0)
                             try {
-                                CoinInfo nc = new CoinInfo(ci.baseSymbol, Double.valueOf(ci.priceUsd), Double.valueOf(t.getText()));
+                                CoinInfo nc = new CoinInfo(ci.baseSymbol, Double.valueOf(ci.priceUsd), Double.valueOf(t.getValue()));
                                 if(userCoins.withdrawCoin(nc,getApiData().getDataArray().data)){
                                     //feito
                                 }else {
@@ -54,11 +79,30 @@ public class WithdrawCoinController extends ControllerClassType{
                             }
                     });
 
+                    JFXButton b2 = new JFXButton();
+                    b2.setText("Retirar tudo");
+                    b2.setOnAction(event -> {
+                        try {
+                            //CoinInfo nc = new CoinInfo(ci.baseSymbol, Double.valueOf(ci.priceUsd), userCoins.getCoinBySymbol(ci.baseSymbol).qtd );
+                            CoinInfo nc = userCoins.getCoinBySymbol(ci.baseSymbol);
+                            if(userCoins.deleteCoin(nc)){
+                                //feito
+                            }else {
+                                messageDialog(coinStackPane, "Operação malsucedida, erro na entrada de dados", "Erro");
+                            }
+                        } catch (Exception e) {
+                            messageDialog(coinStackPane, "Quantia inválida", "Erro");
+                    /*HomeController parentController = (HomeController) getParentController();
+                    parentController.updateJSON();*/
+                        }
+                    });
+
                     p.setAlignment(Pos.CENTER);
                     p.setHgap(10);
                     p.addColumn(1, new Text(ci.baseSymbol));
                     p.addColumn(2, t);
                     p.addColumn(3, b);
+                    p.addColumn(4, b2);
                     c++;
                     listCoins.add(p);
                 }
